9cd8d1cb6cca6c3bb4cbc9d84812b296d66154f6
Deukhyun Park <ppdha82@gmail.com>
2020-07-17 18:59:53 +0900

    [Modify] Add test code

--- Test.py
+++ Test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
+'''
+from openpyxl import load_workbook
+load_wb = load_workbook("./FOCUS_MODEL_DEFINE.xlsx", data_only=True)
+load_ws = load_wb['Sheet1']
 
+
+print(load_ws['A1'].value)
+
+print(load_ws.cell(1, 2).value)
+'''
 import openpyxl
 
 filename = "./FOCUS_DEFINE_MODEL.xlsx"
@@ -17,12 +26,9 @@ if len(key) != 0:
 else:
     print("key is null")
 
-'''while True:'''
 while key is not None:
     key = sheet.cell(section, index).value;
     value = sheet.cell(section, index+1).value;
     print("sheet.cell([row = ", section, ").value = ", key, ":", value)
     section += 1
-    '''if section > 5:
-        break'''
 
