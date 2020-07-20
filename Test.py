68c352210155d05b2f057fa4fb39af26dc4e0268
Deukhyun Park <ppdha82@gmail.com>
2020-07-20 17:28:01 +0900

    [Modify] test code debugging

--- Test.py
+++ Test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 '''
 from openpyxl import load_workbook
 load_wb = load_workbook("./FOCUS_MODEL_DEFINE.xlsx", data_only=True)
@@ -16,10 +16,13 @@ def gen_file(column):
     key = 0
     while key is not None:
         key = sheet.cell(row, column).value
+        print("key = ", key)
         if key == "MODEL":
-            sValue = sheet.cell(row, column).value
+            sValue = sheet.cell(row, column + 1).value
+            print("sValue = ", sValue)
             return open(sValue, 'w')
         row += 1
+    return None
 
 filename = "./FOCUS_DEFINE_MODEL.xlsx"
 book = openpyxl.load_workbook(filename)
@@ -29,13 +32,16 @@ sKey = " "
 section = 1
 index = 1
 
-ini_file = gen_file(index + 1)
+print("RUN gen_file1")
+ini_file = gen_file(index)
+print("RUN gen_file2")
 
 while sKey is not None:
     sKey = sheet.cell(section, index).value
     value = sheet.cell(section, index+1).value
-    if sKey is not None and ini_file.writable() :
+    if sKey is not None and ini_file is not None :
         data = str(sKey) + '=' + str(value) + '\n'
-        ini_file.write(data)
+        if(ini_file.writable()):
+            ini_file.write(data)
     print("sheet.cell([row = ", section, ").value = ", sKey, ":", value)
     section += 1
