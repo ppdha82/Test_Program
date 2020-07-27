2104bf39341a001f35ef4cd56f6318f915964089
Deukhyun Park <ppdha82@focushns.com>
2020-07-27 17:20:20 +0900

    [Modify] converting csv file to ini file

--- Test.py
+++ Test.py
@@ -1,39 +1,14 @@
 #!/usr/bin/env python3
-'''
-from openpyxl import load_workbook
-load_wb = load_workbook("./FOCUS_MODEL_DEFINE.xlsx", data_only=True)
-load_ws = load_wb['Sheet1']
-
-
-print(load_ws['A1'].value)
-
-print(load_ws.cell(1, 2).value)
-'''
-import openpyxl
 import csv
 import os
 
 OEM_PATH = "./define/model_spec/"
 OEM_WHOLE_PATH_NAME = ""
 
-def getOemName(row):
-	column = 1
-	key = 0
-
-	while key is not None:
-		key = sheet.cell(row, column).value
-		print("key = ", key)
-		if key == "OEM":
-			sValue = sheet.cell(row + 1, column).value
-			print("sValue = ", sValue)
-			return str(sValue)
-		column += 1
-
-
 def makeDirectory():
 	try:
 		print("RUN getOemName1")
-		OEM_NAME = getOemName(1)
+		OEM_NAME = a_list[1][0]
 		print("OEM_NAME = ", OEM_NAME)
 		print("RUN getOemName2")
 		global OEM_WHOLE_PATH_NAME
@@ -46,37 +21,25 @@ def makeDirectory():
 			print("Failed to create directory")
 			raise
 
-
 def gen_file(row):
-	column = 1
-	key = 0
-	while key is not None:
-		key = sheet.cell(row, column).value
-		print("key = ", key)
-		if key == "MODEL":
-			print("OEM_WHOLE_PATH_NAME = ", OEM_WHOLE_PATH_NAME)
-			sValue = OEM_WHOLE_PATH_NAME + str("/") + sheet.cell(row + 1, column).value
-			print("sValue = ", sValue)
-			return open(sValue, 'w')
-		column += 1
-	return None
+	print("OEM_WHOLE_PATH_NAME = ", OEM_WHOLE_PATH_NAME)
+	sValue = OEM_WHOLE_PATH_NAME + str("/") + a_list[1][1]
+	print("sValue = ", sValue)
+	return open(sValue, 'w')
 
-
-xlsxFilename = "./FOCUS_DEFINE_MODEL.xlsx"
 csvFilename = "./FOCUS_DEFINE_MODEL.csv"
+a_list = []
 
-book = openpyxl.load_workbook(xlsxFilename)
-sheet = book['Sheet2']
 workfile = open(csvFilename, 'r', encoding='utf-8')
 rdCsv = csv.reader(workfile)
 
 for line in rdCsv:
+	a_list.append(line)
 	print("line = ", line)
 
+print("a_list[0][0] = ", a_list[0][0], "; a_list[1][1] = ", a_list[1][1])
 workfile.close()
 
-sKey = " "
-section = 1
 index = 1
 
 print("RUN makeDirectory1")
@@ -86,12 +49,14 @@ print("RUN gen_file1")
 ini_file = gen_file(index)
 print("RUN gen_file2")
 
-while sKey is not None:
-	sKey = sheet.cell(section, index).value
-	value = sheet.cell(section + 1, index).value
-	if sKey is not None and ini_file is not None:
-		data = str(sKey) + '=' + str(value) + '\n'
-		if(ini_file.writable()):
-			ini_file.write(data)
-	print("sheet.cell([row = ", section, ", column = ", index, ").value = ", sKey, ":", value)
-	index += 1
+''' ARCH, SENSOR_TYPE, IPDEVICE_TYPE, SUB_MODEL, SENSOR, CDS_MODE, IR_PWM, SUPPORT_H265, SUPPORT_MFZ, SUPPORT_PIRIS, ALPUC, USE_LPR, DDNS, DTON, NTOD '''
+columnIndex = 0
+keyLen = len(a_list[0]) - 2
+print("keyLen = ", keyLen)
+while columnIndex < keyLen:
+	valueIndex = columnIndex + 2
+	if(ini_file.writable()):
+		data = a_list[0][valueIndex] + ' = ' + a_list[1][valueIndex] + '\n'
+		ini_file.write(data)
+	print("a_list[", columnIndex, "] = ", a_list[1][valueIndex])
+	columnIndex += 1
