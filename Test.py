94ba5217188e95194ddafe2a128ecd094d6eb159
Deukhyun Park <ppdha82@focushns.com>
2020-08-12 00:40:18 +0900

    [Modify] fix to enter value instead key while converting ini file to csv format. fix to write last index(OEM_NSOK) while converting csv format to ini file.

--- Test.py
+++ Test.py
@@ -9,10 +9,8 @@ model_count = 0
 
 def makeDirectory(row):
 	try:
-		print("RUN getOemName1")
 		OEM_NAME = a_list[row][0]
 		print("OEM_NAME = ", OEM_NAME)
-		print("RUN getOemName2")
 		global OEM_WHOLE_PATH_NAME
 		OEM_WHOLE_PATH_NAME = OEM_PATH + OEM_NAME
 		print("OEM_WHOLE_PATH_NAME = ", OEM_WHOLE_PATH_NAME)
@@ -41,15 +39,13 @@ def main():
 		a_list.append(line)
 		print("line = ", line)
 
-	print("a_list[0][0] = ", a_list[0][0], "; a_list[1][1] = ", a_list[1][1])
+	#print("a_list[0][0] = ", a_list[0][0], "; a_list[1][1] = ", a_list[1][1])
 	workfile.close()
 
 	index = 1
 
 	while index < model_count:
-		print("RUN makeDirectory1")
 		makeDirectory(index)
-		print("RUN makeDirectory2")
 		print("RUN gen_file1(" + str(index) + ")")
 		ini_file = gen_file(index)
 		print("RUN gen_file2")
@@ -61,9 +57,9 @@ def main():
 		while columnIndex < keyLen:
 			valueIndex = columnIndex + 2
 			if(ini_file.writable()):
-				data = a_list[index - 1][valueIndex] + '=' + a_list[1][valueIndex] + '\n'
+				data = a_list[0][valueIndex] + '=' + a_list[index][valueIndex] + '\n'
 				ini_file.write(data)
-			print("a_list[", columnIndex, "] = ", a_list[index][valueIndex])
+			print("a_list[", columnIndex, "] = " + a_list[index][valueIndex] + "; key = " + a_list[0][valueIndex])
 			columnIndex += 1
 		index += 1
 
--- TestRev.py
+++ TestRev.py
@@ -17,31 +17,31 @@ def move_to_base_dir():
 	cur_pwd = os.getcwd()
 	#print(cur_pwd)
 	os.chdir("./define")
-	#print("current dir = ", os.getcwd())
+	#print("current dir = " + os.getcwd())
 
 def move_to_dir(target):
 	pwd = print(os.getcwd())
-	#print("pwd+target = ", pwd + target)
+	#print("pwd+target = " + pwd + target)
 	os.chdir(pwd + target)
 	#print(os.getcwd())
 
 def move_to_main_dir():
 	#print(os.getcwd())
 	os.chdir("..")
-	#print("current dir = ", os.getcwd())
+	#print("current dir = " + os.getcwd())
 
 def get_list_in_dir():
 	''' find dir and file in sub_dir '''
 	global cur_pwd
 	cur_pwd = os.getcwd()
 	cur_pwd_list = os.listdir(cur_pwd)
-	#print("cur_pwd_list = ", cur_pwd_list, "; type = ", type(cur_pwd_list))
+	#print("cur_pwd_list = " + str(cur_pwd_list) + "; type = " + str(type(cur_pwd_list)))
 	return cur_pwd_list
 
 def get_list_in_target_dir(path, target):
 	''' find dir and file in sub_dir '''
 	cur_pwd_list = os.listdir(path + "/" + target)
-	#print("cur_pwd_list = ", cur_pwd_list, "; type = ", type(cur_pwd_list))
+	#print("cur_pwd_target_list = " + str(cur_pwd_list) + "; type = " + str(type(cur_pwd_list)))
 	return cur_pwd_list
 
 def seperate_file_dir(cur_pwd_list):
@@ -55,7 +55,7 @@ def seperate_file_dir(cur_pwd_list):
 			dir_list = cur_pwd + "/" + x
 		else:
 			result = "unknown"
-		#print(x, " is ", result)
+		#print(x + " is " + result)
 
 def run():
 	global sub_dir_list
@@ -65,7 +65,7 @@ def run():
 	move_to_base_dir()
 
 	sub_dir_list = get_list_in_dir()
-	#print("sub_dir_list = ", sub_dir_list)
+	#print("sub_dir_list = " + sub_dir_list)
 	seperate_file_dir(sub_dir_list)
 
 	''' deal with file '''
@@ -73,19 +73,15 @@ def run():
 		print("Exit Program")
 		return False
 
-#	csvFilename = "./FOCUS_DEFINE_MODEL_TEST.csv"
-#	workfile = open(csvFilename, 'w', encoding='utf-8')
-#	workfile.close()
-
-	#print("list = ", dir_list)
+	#print("list = " + dir_list)
 	sub_dir_list = os.listdir(dir_list)
-	'''
-	print("sub_dir_list =", sub_dir_list)
+
+	#print("sub_dir_list = " + sub_dir_list)
 	index = 1
 	for a in sub_dir_list:
-		print("sub_dir_list[", index, "] = ", a)
+		#print("sub_dir_list[" + index + "] = " + a)
 		index += 1
-	'''
+
 	dev_index = 0
 	index = 1
 	data = sub_dir_list.pop(0)
@@ -95,23 +91,23 @@ def run():
 	# initialize key about OEM & MODEL
 	header_list = ["OEM", "MODEL"]
 	value_list = [[""], [""],]
-	while len(sub_dir_list) > 0:
-		print("data[" + str(index) + "] = ", data)
+	while len(sub_dir_list) >= 0:
+		print("data[" + str(index) + "] = " + data)
 		ini_file_list = get_list_in_target_dir(dir_list, data)
 		path = dir_list + "/" + data
 
-		print("[check] path = ", path)
-		print("[check] ini_file_list = ", ini_file_list)
+		print("[check] path = " + path)
+		print("[check] ini_file_list = " + str(ini_file_list))
 		if len(ini_file_list) > 0:
 			ini_index = 1
 			for ini_data in ini_file_list:
 				found_value_index = 0
 				for header in header_list:
 					if "OEM" == header:
-						print("header (" + header + ") is found(key = OEM, (" + str(dev_index) + ", " + str(found_value_index) + "))")
-						print("len(value_list) = " + str(len(value_list)) + "; found_value_index = " + str(found_value_index))
+						#print("header(" + header + ") is found(key = OEM, (" + str(dev_index) + ", " + str(found_value_index) + "))")
+						#print("len(value_list) = " + str(len(value_list)) + "; found_value_index = " + str(found_value_index))
 						if len(value_list) <= dev_index:
-							print("add list to value_list [OEM]")
+							#print("add list to value_list [OEM]")
 							value_list.append([""])
 						# add OEM value
 						value_list[dev_index][found_value_index] = data
@@ -121,10 +117,10 @@ def run():
 				found_value_index = 0
 				for header in header_list:
 					if "MODEL" == header:
-						print("header (" + header + ") is found(key = MODEL, (" + str(dev_index) + ", " + str(found_value_index) + "))")
-						print("len(value_list) = " + str(len(value_list)) + "; found_value_index = " + str(found_value_index))
+						#print("header(" + header + ") is found(key = MODEL, (" + str(dev_index) + ", " + str(found_value_index) + "))")
+						#print("len(value_list) = " + str(len(value_list)) + "; found_value_index = " + str(found_value_index))
 						if len(value_list[dev_index]) <= found_value_index:
-							print("add list to value_list [MODEL]")
+							#print("add list to value_list [MODEL]")
 							value_list[dev_index].append("")
 						# add MODEL value
 						value_list[dev_index][found_value_index] = ini_data
@@ -132,21 +128,21 @@ def run():
 					found_value_index += 1
 				print("[value] ini_file_list[" + str(ini_index) + "] = " + ini_data)
 				sub_path_file = path + "/" + ini_data
-				print("sub_path_file =", sub_path_file)
+				print("sub_path_file = " + sub_path_file)
 				read_file = open(sub_path_file, 'r')
-				print("read_file  >>>>>>")
+				#print("read_file  >>>>>>")
 				read_all_line = read_file.read().splitlines()
-				print(read_all_line)
+				#print(read_all_line)
 
 				for read_line in read_all_line:
-					print("read_line >>>>> " + read_line)
+					#print("read_line >>>>> " + read_line)
 					if len(read_line) <= 0:
 						break
 					slices_line = read_line.split('=')
 					slice_key = slices_line[0]
 					slice_value = slices_line[1]
 					if not slice_key in header_list:
-						print("add " + slice_key + " in header_list")
+						#print("add " + slice_key + " in header_list")
 						header_list.append(slice_key)
 					found_index = 0
 					for header in header_list:
@@ -160,22 +156,26 @@ def run():
 									while len(value_list[apply_index]) <= found_index:
 										value_list[apply_index].append("0")
 										if max_column <= found_index:
-											print("max_column = " + str(max_column) + "; found_index = " + str(found_index))
+											if max_column < found_index:
+												print("max_column = " + str(max_column) + "; found_index = " + str(found_index))
 											max_column = found_index
 									apply_index += 1
 							#print("header_list = " + str(header_list))
-							print("header (" + header + ") is found(slice_key = " + slice_key + ", " + str(found_index) + "/" + str(max_column) + ")")
-							print("len(value_list[" + str(dev_index) + "]) = " + str(len(value_list[dev_index])) + "; dev_index = " + str(dev_index), "found_index = " + str(found_index))
+							#print("header (" + header + ") is found(slice_key = " + slice_key + ", " + str(found_index) + "/" + str(max_column) + ")")
+							#print("len(value_list[" + str(dev_index) + "]) = " + str(len(value_list[dev_index])) + "; dev_index = " + str(dev_index), "found_index = " + str(found_index))
 							value_list[dev_index][found_index] = slice_value
-							print("value_list[" + str(dev_index) + "] = " + str(value_list[dev_index]))
+							#print("value_list[" + str(dev_index) + "] = " + str(value_list[dev_index]))
 							break
 						found_index += 1
 					csv_index += 1
 				while len(value_list[dev_index]) <= max_column:
-					print("len(value_list[" + str(dev_index) + "] = " + str(len(value_list[dev_index])))
+					#print("len(value_list[" + str(dev_index) + "] = " + str(len(value_list[dev_index])))
 					value_list[dev_index].append("0")
 				ini_index += 1
 				dev_index += 1
+		if len(sub_dir_list) <= 0:
+			print("sub_dir_list size is 0")
+			break
 		data = sub_dir_list.pop(0)
 		index += 1
 
@@ -185,7 +185,10 @@ def run():
 		wr_csv.writerow(value_list[print_index])
 		print_index += 1
 	workfile.close()
-	print("header_list = ", header_list)
-	print("value_list = ", value_list)
+	print("header_list = " + str(header_list))
+	print_index = 0
+	for idx in value_list:
+		print("value_list[" + str(print_index) + "] = " + str(idx))
+		print_index += 1
 
 run()
