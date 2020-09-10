3aa0952502723a5173769ce590a3135c0635840f
Deukhyun Park <ppdha82@gmail.com>
2020-09-10 18:40:35 +0900

    [Modify] improve function
    prevent to write duplicately keyword to Excel
    append comment to source code

--- read_htm.py
+++ read_htm.py
@@ -8,11 +8,13 @@ testFilename = "test.txt"
 
 REMOVAL_WORD = [ "_PRC", "_FRE", "_GER", "_KOR", "_JPN", "_CHI", "_ENG"]
 ENCODING = [ "BIG5-HKSCS", "ISO-8859-1", "ISO-8859-2", "EUC-KR", "EUC-JP", "EUC-CN", "UTF-8" ]
+
 book = openpyxl.Workbook()
 sheet = book.active
 sheet.title = 'Sheet1'
 column = 'A'
-row = '1'
+keyColumn = 'A'
+row = '2'
 testFile = open(testFilename, "w")
 
 cur_pwd = os.getcwd() + "/temp"
@@ -20,6 +22,7 @@ cur_pwd_list = os.listdir(cur_pwd)
 print("cur_pwd_list = " + str(cur_pwd_list))
 dir = cur_pwd
 removalWord = ""
+keyArray = []
 
 for htmFile in cur_pwd_list:
 	next_column = chr(ord(column) + 1)
@@ -37,9 +40,9 @@ for htmFile in cur_pwd_list:
 				for string in charsetSplit:
 					if "charset=" in string:
 						if len(string) == len("charset="):
-							charset = charsetSplit[index + 1]
+							charset = charsetSplit[index + 1].upper()
 						else:
-							charset = string.replace("charset=", "", -1)
+							charset = string.replace("charset=", "", -1).upper()
 						break
 					index += 1
 				print("charset = " + charset)
@@ -49,6 +52,7 @@ for htmFile in cur_pwd_list:
 	for key in ENCODING:
 		if key == charset:
 			removalWord = REMOVAL_WORD[idx]
+			sheet[column + '1'] = REMOVAL_WORD[idx]
 			print("removalWord = " + removalWord)
 		idx += 1
 
@@ -61,11 +65,10 @@ for htmFile in cur_pwd_list:
 				#print("foundIndex = " + str(foundIndex) + "; key = " + key)
 				break
 
-
 	with open(dir + "/" + htmFile, "rb") as input:
 		for data in input.readlines():
-			print("data = " + str(data))
-			pos = str(column + row)
+			needToAppend = 1
+			#print("data = " + str(data))
 			mid_data = data.decode(encoding=charset).rstrip()
 			testFile.write(str(mid_data))
 			if len(str(mid_data)) >= 2:
@@ -73,22 +76,35 @@ for htmFile in cur_pwd_list:
 			mid2_data = mid_data.split("\"")
 			if(len(mid2_data) < 2):
 				continue
-			print("mid2_data = " + str(mid2_data))
+			#print("mid2_data = " + str(mid2_data))
 
 			mid3_data = mid2_data[0].split(" ")
-			print("mid3_data = " + str(mid3_data))
+			#print("mid3_data = " + str(mid3_data))
 			if(len(mid3_data) < 2):
 				continue
 			mid4_data = mid3_data[1].replace("\t", "", -1)
 			last_data = str(mid4_data).replace(removalWord, "", -1)
 			print("last_data = " + last_data)
-			sheet[pos] = last_data
+			if len(keyArray) < int(row):
+				keyArray.append(last_data)
+				print("Enter = " + last_data + "(" + str(len(keyArray) - 1) + ")")
+			else:
+				idx = 0
+				while len(keyArray) > idx:
+					if keyArray[idx] == last_data:
+						print("Found same keyword idx = " + str(idx) + "(" + last_data + ")")
+						needToAppend = 0
+					idx += 1
+			print("len(keyArray) = " + str(len(keyArray)))
+			if needToAppend == 1:
+				pos = str(column + row)
+				sheet[pos] = last_data
 			next_pos = str(next_column) + str(row)
 			sheet[next_pos] = mid2_data[1]
 			print("mid2_data[1] = " + str(mid2_data[1]))
 			row = str(int(row) + 1)
 			#print("dec(data) = " + data.decode(encoding='big5-hkscs'))
-	row = '1'
+	row = '2'
 	column = chr(ord(column) + 2)
 
 
