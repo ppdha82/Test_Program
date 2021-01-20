0a8e48c161a258cdfee97eb4140acf6830d6167c
Deukhyun Park <ppdha82@gmail.com>
2021-01-20 15:56:17 +0900

    [Modify] YUV to RGB example code
    convert YUV to RGB and sample capture

---
+++ debug_print.h
@@ -0,0 +1,36 @@
+#ifndef __DEBUG_PRINT_H__
+#define __DEBUG_PRINT_H__
+
+#define _C_BLACK 	"\033[30m"
+#define _C_RED 		"\033[31m"
+#define _C_GREEN 	"\033[32m"
+#define _C_YELLOW 	"\033[33m"
+#define _C_BLUE 	"\033[34m"
+#define _C_MAGENTA	"\033[35m"
+#define _C_CYAN 	"\033[36m"
+#define _C_WHITE 	"\033[37m"
+#define _C_END 		"\033[0m"
+#define _C_YELLOWRED	"\033[1;33;41m"
+
+#define debug_printf(color, fmt, arg...) \
+	do { \
+		fprintf(stderr, color "%s:%s:%d>" _C_END " " fmt , __FILE__, __func__, __LINE__, ##arg); \
+	} while(0)
+
+#if !defined (__DEBUG_ON__)
+#define DBG(fmt, arg...)
+#define DBG_R(fmt, arg...)
+#define DBG_G(fmt, arg...)
+#define DBG_B(fmt, arg...)
+#define DBG_Y(fmt, arg...)
+#define DBG_C(fmt, arg...)
+#else	/* __DEBUG_ON__ */
+#define DBG(fmt, arg...) debug_printf(_C_END, fmt, ##arg)
+#define DBG_R(fmt, arg...) debug_printf(_C_RED, fmt, ##arg)
+#define DBG_G(fmt, arg...) debug_printf(_C_GREEN, fmt, ##arg)
+#define DBG_B(fmt, arg...) debug_printf(_C_BLUE, fmt, ##arg)
+#define DBG_Y(fmt, arg...) debug_printf(_C_YELLOW, fmt, ##arg)
+#define DBG_C(fmt, arg...) debug_printf(_C_CYAN, fmt, ##arg)
+#endif	/* __DEBUG_ON__ */
+
+#endif	/* __DEBUG_PRINT_H__ */
