f7105c9aec925650d28d4b8168a1bd3c1019bb5d
Deukhyun Park <ppdha82@gmail.com>
2022-03-21 14:12:35 +0900

    [Add] file lock 테스트 코드 추가

--- c_source/test_fnctl.c
+++ c_source/test_fnctl.c
@@ -22,6 +22,19 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+#include <pthread.h>
+
+#include "debug_print.h"
+
+typedef struct _FILE_INFO {
+    int fd;
+} FILE_INFO;
+
+enum {
+    FILE_LOCK_TIMEOUT = 10,
+    FILE_LOCK_TEST = 15,
+};
+
 // 파일이 잠겨 있는지 확인해서 잠금을 얻고 
 // 잠겨 있을 경우 잠금이 풀릴 때까지 
 // 기다린다.  
@@ -51,36 +64,74 @@ int fd_unlock(int fd)
     return fcntl(fd, F_SETLK, &lock);
 }
 
+void* openfile(void *arg)
+{
+    FILE_INFO* pFInfo = (FILE_INFO*)arg;
+    int ret = 0;
+    int count = FILE_LOCK_TEST;
+
+    _DBG_R("pFInfo->fd = %d\n", pFInfo->fd);
+    while(count > 0) {
+        ret = fd_lock(pFInfo->fd);
+        if(ret < 0) {
+            _DBG_R("Failed to lock fd(%d) count:%d\n", pFInfo->fd, count);
+        }
+        else {
+            _DBG_R("Success to lock fd(%d) count:%d\n", pFInfo->fd, count);
+        }
+        count--;
+        sleep(1);
+    }
+
+    return (void *)&pFInfo->fd;
+}
+
 int main(int argc, char **argv)
 {
+    /* pthread */
+   	pthread_t t1;
+	int n = 10;
+	int *ret;
+
+    /* file lock */
     int fd;
     int id;
     int i;
+    FILE_INFO fInfo;
 
-    char *file_name = "lock.file";
+    const char *file_name = "./lock.file";
 	if(argc < 2) {
 		perror("few parameter");
 		exit(0);
 	}
 
     id = atoi(argv[1]);
-    if ((fd = open(file_name, O_RDWR)) < 0)
-    {
+    fd = open(file_name, O_RDWR | O_CREAT, 0644);
+    if(fd < 0) {
         perror("open failure");
         exit(0);
     }
-    if (fd_lock(fd) == -1)
-    {
+
+    fInfo.fd = fd;
+    _DBG_G("fd = %d(%d)\n", fd, fInfo.fd);
+
+    if (fd_lock(fd) == -1) {
         perror("fd lock error");
         exit(0);
     }
-    for (i = 0; i < 10; i++)
-    {
+
+	pthread_create(&t1, NULL, openfile, &fInfo);
+	pthread_detach(t1);
+    for (i = 0; i < FILE_LOCK_TIMEOUT; i++) {
+        lseek(fd, 0, SEEK_SET);
+        read(fd, )
         printf("file lock %d : %d\n", id, i);
         sleep(1);
     }
-    if (fd_unlock(fd) == -1)
-    {
+	pthread_join(t1, (void**)&ret);
+    _DBG_R("*ret = %d\n", *ret);
+
+    if (fd_unlock(fd) == -1) {
         perror("fd unlock error");
         exit(0);
     }
