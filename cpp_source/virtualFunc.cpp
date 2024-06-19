33024ad8723a959bbcdcef6939ec4732ec4172d4
Deukhyun Park <ppdha82@gmail.com>
2024-06-19 09:59:54 +0900

    [] cpp 예제 코드 추가
    - test 파일에서 virtual 예제 코드 삭제
    - virtual 함수 에제 별도 파일에 생성

---
+++ cpp_source/virtualFunc.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <cstdio>
+
+// g++ -o test test.cpp
+
+using namespace std;
+
+class BaseInterface
+{
+public:
+    virtual void pureVFunc() = 0;
+};
+
+class Base : public BaseInterface
+{
+public:
+    void pureVFunc()
+    {
+        cout << "[pure virtual] " << __func__ << endl;
+    }
+
+    void func()
+    {
+        cout << "Base func" << endl;
+    }
+
+    void func(int a, int b)
+    {
+        cout << "[int] a + b = " << a + b << endl;
+    }
+
+    void func(double a, double b)
+    {
+        cout << "[double] a + b = " << a + b << endl;
+    }
+
+    virtual void vfunc1()
+    {
+        cout << "virtual base" << __func__ << endl;
+    }
+
+    virtual void vfunc2()
+    {
+        cout << "virtual base" << __func__ << endl;
+    }
+
+    void vfunc3()
+    {
+        cout << "virtual base" << __func__ << endl;
+    }
+
+    void vfunc4()
+    {
+        cout << "virtual base" << __func__ << endl;
+    }
+};
+
+class Derived : public Base
+{
+public:
+    void func()
+    {
+        cout << "Derived func" << endl;
+    }
+
+    virtual void vfunc1()
+    {
+        cout << "virtual derived" << __func__ << endl;
+    }
+
+    void vfunc2()
+    {
+        cout << "virtual derived" << __func__ << endl;
+    }
+
+    virtual void vfunc3()
+    {
+        cout << "virtual derived" << __func__ << endl;
+    }
+
+    void vfunc4()
+    {
+        cout << "virtual derived" << __func__ << endl;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    Base a;
+    Derived b;
+    Base *pA;
+
+    cout << "\n[overload 예제]" << endl;
+
+    a.func(1, 2);
+    a.func(2.0, 3.3);
+
+    cout << "\n[override 예제]" << endl;
+
+    a.func();
+    b.func();
+
+    cout << "\n[virtual 예제]" << endl;
+
+    pA = &b;
+    a.vfunc1();
+    pA->vfunc1();
+
+    cout << "\n";
+
+    a.vfunc2();
+    pA->vfunc2();
+
+    cout << "\n";
+
+    a.vfunc3();
+    pA->vfunc3();
+
+    cout << "\n";
+
+    a.vfunc4();
+    pA->vfunc4();
+
+    a.pureVFunc();
+    pA->pureVFunc();
+
+    return 0;
+}
